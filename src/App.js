48d826d8ba6c470c8b886b8252dcf234cfdb7e5b
gereReda <gderbew2@gmail.com>
2024-01-29 01:44:41 +0300

    basic layout page made

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [userName,setuserName] = useState("")
+  const [password,setPassword] = useState("")
+  const handleSubmit = (e) => {
+
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <img className='bgimg' src='BG.svg' alt='background-img'/>
+      <div className='form-container'>
+        <img className='img-grp' src='Group.svg' alt='cart-group-img'/>
+        <form onSubmit={handleSubmit} className='form'>
+          <div className='input-block'>
+            <img src='user.svg' className='img' alt='user-icon-img'/>
+            <input value={userName} onChange={(enVal)=>setuserName(enVal.target.value)} placeholder='USERNAME' className='text-input'/>
+          </div>
+          <div className='input-block'>
+            <img src='lock.svg' className='img' alt='password-lock-icon'/>
+            <input value={password} onChange={(enVal)=>setPassword(enVal.target.value)} placeholder='PASSWORD' className='text-input'/>
+          </div>
+          <input type='submit' value={"LOGIN"} className='login'/>
+          <div className='forgot'>Forgot Password?</div>
+        </form>
+      </div>
     </div>
   );
 }
